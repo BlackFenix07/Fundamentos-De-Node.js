aef52a66e89f51885f90bc7a9a70b60409dd8c10
Andrés Zapata <andresmauricio030400@gmail.com>
2023-06-02 22:24:06 -0500

    Clase de console

---
+++ modulos/03.consola.js
@@ -0,0 +1,65 @@
+// console.log("Registro");
+// console.info("Información");
+// console.warn("Alerta");
+// console.error("Error");
+// console.table("Tabla");
+// console.group("Grupo");
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// var tabla = [
+//   {
+//     a: 1,
+//     b: "Y",
+//   },
+//   {
+//     a: 2,
+//     b: "Z",
+//   },
+// ];
+
+// console.table(tabla);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// console.group("Conversación");
+// console.log("Hola Andrés");
+// console.group("Hablar");
+// console.log("Hablando...");
+// console.log("Hablando...");
+// console.log("Hablando...");
+// console.groupEnd("Hablar");
+// console.log("Adiós Andrés");
+// console.groupEnd("Conversación");
+// console.log("Otra conversación");
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// function function1() {
+//   console.group("Función 1");
+//   console.log("Esto pertenece a la función 1");
+//   console.log("Esto pertenece a la función 1");
+//   console.log("Esto pertenece a la función 1");
+//   function2();
+//   console.log("Hemos vuelto a la función 1");
+//   console.groupEnd("Función 1");
+// }
+
+// function function2() {
+//   console.group("Función 2");
+//   console.log("Esto pertenece a la función 2");
+//   console.log("Esto pertenece a la función 2");
+//   console.log("Esto pertenece a la función 2");
+//   console.groupEnd("Función 2");
+// }
+
+// console.log("Empezamos");
+// function1();
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+console.count("Veces");
+console.count("Veces");
+console.count("Veces");
+console.count("Veces");
+console.count("Veces");
+console.countReset("Veces");
+console.count("Veces");
+console.count("Veces");
+console.count("Veces");
