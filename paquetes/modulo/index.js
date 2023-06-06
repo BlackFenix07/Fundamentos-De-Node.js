6bbf3e39f866bdbd40bd5c001634c5365c3383dd
Andrés Zapata <andresmauricio030400@gmail.com>
2023-06-05 22:12:38 -0500

    Clase de construcción de módulos, require e import

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,7 @@
+// Se trae el módulo
+const modulo = require("./modulo");
+
+// Se ejecuta la función del módulo
+modulo.saludar();
+console.log(modulo.prop1);
+console.log(modulo.prop2);
---
+++ paquetes/modulo/modulo.js
@@ -0,0 +1,9 @@
+function saludar() {
+  console.log("Hola");
+}
+
+module.exports = {
+  saludar,
+  prop1: "Hola mundo",
+  prop2: "Adiós mundo",
+};
